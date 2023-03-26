5d40df88eaaedf5af1dbcba0726b975d6b0b4de8
zachary bordelon <zackmbordelon@gmail.com>
2023-03-26 18:42:44 -0500

    activities 90% passing

--- db/activities.js
+++ db/activities.js
@@ -12,7 +12,7 @@ async function createActivity({ name, description }) {
     RETURNING *;
     
     `,[name, description]) 
-    return rows;
+    return rows[0];
   }
   catch (err){
     console.error(err)
@@ -35,17 +35,63 @@ async function getAllActivities() {
   }
 }
 
-async function getActivityById(id) {}
+async function getActivityById(id) {
 
-async function getActivityByName(name) {}
+try {
+  const { rows } = await client.query(`
+  
+    SELECT * FROM activities
+    WHERE id = ${id};
+
+  `);
+  return rows[0]
+} catch(err){
+  console.error(err)
+}
+
+}
+
+async function getActivityByName(name) {
+
+  try {
+    const { rows } = await client.query(`
+    
+      SELECT * FROM activities
+      WHERE name = '${name}';
+  
+    `);
+    return rows[0]
+  } catch(err){
+    console.error(err)
+  }
+
+}
 
 // used as a helper inside db/routines.js
 async function attachActivitiesToRoutines(routines) {}
 
+
+
+// WE ARE HERE AND ITS BROKEN!!!!!!!!!!!!!!!!!!!!!!!!
 async function updateActivity({ id, ...fields }) {
   // don't try to update the id
   // do update the name and description
   // return the updated activity
+
+console.log("FIELDS LOGGED HERE",fields)
+try {
+  const { rows } = await client.query(`
+  
+    UPDATE activities
+    SET name = '${fields.name}', description = '${fields.description}'
+    WHERE id = '${id}'
+    RETURNING *;
+  `);
+  return rows[0];
+} catch(err) {
+  console.error(err)
+}
+
 }
 
 module.exports = {
--- db/users.js
+++ db/users.js
@@ -13,22 +13,54 @@ async function createUser({ username, password }) {
     RETURNING *;
     `, [username, password]);
   delete rows[0].password;
-  return rows;
+  return rows[0];
   } catch(err){
     console.error(err)
   }
 }
-
+//we are here
 async function getUser({ username, password }) {
+ try {
+  const { rows } = await client.query(`
 
+  SELECT * FROM users
+  WHERE username = '${username}';
+
+  `)
+  if (password === rows[0].password)  {
+    delete rows[0].password;
+    return rows[0];
+  }
+ } catch (err){
+  console.error(err)
+  }
 }
 
 async function getUserById(userId) {
-
+  try {
+    const { rows } = await client.query(`
+  
+    SELECT * FROM users
+    WHERE id = ${userId};
+  
+    `)
+    delete rows[0].password;
+    return rows[0];
+  } catch(err) {
+  console.error(err)
+ }
 }
 
 async function getUserByUsername(userName) {
-
+  try{ 
+    const { rows } = await client.query(`
+      SELECT * FROM users 
+      WHERE username = '${userName}';
+    `) 
+    return rows[0];
+  } catch(err) {
+    console.error(err)
+  }
 }
 
 module.exports = {
